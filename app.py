d6c0b16d035ec25415189e247f3f2a3f358a1ee2
DeogHoon Hwang <harry.d.hwang@DeogHoons-MacBook-Air.local>
2024-07-14 02:31:14 +0900

    Flask Finished, OpenAI API Done

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 from flask import Flask, request, jsonify
+import logging
 from flask_cors import CORS
 from pytube import YouTube
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
 import textwrap
 import openai
+import os
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-# openai.api_key ="Type in your keys"
+openai_api_key = os.getenv("OPENAI_API_KEY")
+print(f"API Key: {openai_api_key}")  # Add this line for debugging
+if not openai_api_key:
+    app.logger.error(
+        "OpenAI API key not found. Please set the OPENAI_API_KEY environment variable."
+    )
+openai.api_key = openai_api_key
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 def get_video_id(video_url):
@@ -29,7 +39,7 @@ def get_transcript():
         text_formatted = text_formatter.format_transcript(transcript)
         text_info = text_formatted.replace("\n", " ")
         shorten_text_info = textwrap.shorten(
-            text_info, 200, placeholder=" [..이하 생략..]"
+            text_info, 500, placeholder=" [..이하 생략..]"
         )
 
         response_data = {
@@ -39,6 +49,7 @@ def get_transcript():
 
         response = jsonify(response_data)
         response.headers["Content-Type"] = "application/json"
+        print(response)
         return response
     except Exception as e:
         return jsonify({"error": str(e)}), 500
@@ -54,7 +65,7 @@ def ask_openai():
         return jsonify({"error": "Prompt and question information are required"}), 400
 
     try:
-        response = openai.ChatCompletion.create(  # type: ignore
+        response = openai.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
                 {
@@ -63,9 +74,9 @@ def ask_openai():
                 },
                 {"role": "user", "content": f"{prompt}\n\n{question_info}"},
             ],
-            max_tokens=300,
+            max_tokens=800,
         )
-        answer = response.choices[0].message["content"].strip()
+        answer = response.choices[0].message.content
 
         response_data = {
             "answer": answer,
@@ -73,8 +84,10 @@ def ask_openai():
 
         response = jsonify(response_data)
         response.headers["Content-Type"] = "application/json"
+        print(response)
         return response
     except Exception as e:
+        app.logger.error(f"Error in ask_openai: {str(e)}")
         return jsonify({"error": str(e)}), 500
 
 
